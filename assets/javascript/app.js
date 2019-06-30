b008021b60be6e4d91486cecccc366caa1728f0e
Alex Chu <chu.alex9102@gmail.com>
2019-06-30 14:04:17 -0400

    added firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,18 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCL-RRX9a62sv2w7V_M9Zjg1tXxTGb6rdc",
+  authDomain: "find-in-five.firebaseapp.com",
+  databaseURL: "https://find-in-five.firebaseio.com",
+  projectId: "find-in-five",
+  storageBucket: "",
+  messagingSenderId: "325575958489",
+  appId: "1:325575958489:web:0c9af4f6893f24d8"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+
 var movieArray = [];
 var netflixMovies = [];
 $("#movieCall").on("click", function () {
